f119eea24c1e13744f07c6c57d33eb0aef274382
Thomas Massmann <thomas@propertyshelf.com>
2014-02-01 11:50:45 +0100

    PEP8.

--- src/pspolicy/homes4/base/tests/test_setup.py
+++ src/pspolicy/homes4/base/tests/test_setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest2 as unittest
 
 from Products.CMFCore.utils import getToolByName
@@ -9,17 +10,17 @@ from pspolicy.homes4.base.testing import\
 class TestExample(unittest.TestCase):
 
     layer = PSPOLICY_HOMES4_BASE_INTEGRATION
-    
+
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
-    
+
     def test_product_is_installed(self):
-        """ Validate that our products GS profile has been run and the product 
-            installed
-        """
+        """Validate that our product is installed."""
         pid = 'pspolicy.homes4.base'
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
-        self.assertTrue(pid in installed,
-                        'package appears not to have been installed')
+        self.assertTrue(
+            pid in installed,
+            'package appears not to have been installed',
+        )
